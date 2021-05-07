6bb29965b9c6cec00cd81f329075298fd3b3b927
Atomm94 <atom.makaryan.94@gmail.com>
2021-05-07 12:15:43 +0400

    first commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import {token} from "./Helpers/auth";
 import Passport from "./passport";
 
 const app = express();
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
