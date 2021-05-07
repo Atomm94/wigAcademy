d513815c87a5da5ebe47e24a9c38631b6cb6be41
Atomm94 <atom.makaryan.94@gmail.com>
2021-05-07 11:11:56 +0400

    first commit

---
+++ app-compiled.js
@@ -0,0 +1,52 @@
+"use strict";
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+var _express = _interopRequireDefault(require("express"));
+
+var _cors = _interopRequireDefault(require("cors"));
+
+var _passport = _interopRequireDefault(require("passport"));
+
+var _bodyParser = _interopRequireDefault(require("body-parser"));
+
+var config = _interopRequireWildcard(require("./config"));
+
+var _routes = _interopRequireDefault(require("./Api/routes"));
+
+var _auth = require("./Helpers/auth");
+
+var _passport2 = _interopRequireDefault(require("./passport"));
+
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var app = (0, _express["default"])();
+var port = 5000 || process.env.PORT;
+app.use((0, _cors["default"])());
+app.use(_bodyParser["default"].json());
+app.use(_bodyParser["default"].urlencoded({
+  extended: true
+}));
+app.use(_express["default"]["static"](__dirname + '/Media'));
+app.use('/api/user/log', _auth.token);
+app.use('/api/admin/log', _auth.token);
+app.use('/api', _routes["default"]);
+app.use(require('cookie-parser')());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+(0, _passport2["default"])(_passport["default"]);
+app.get('/google', function (req, res) {
+  res.status(200).json({
+    msg: 'authorize success!'
+  });
+});
+app.listen(port, function () {
+  console.log("Server started with port ".concat(port));
+});
