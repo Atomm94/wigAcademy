6fd3d897a5a753f8e3500fa323498f0fa04ffc08
Atomm94 <atom.makaryan.94@gmail.com>
2021-06-07 15:03:52 +0400

    new update

--- Api/Course/router.js
+++ Api/Course/router.js
@@ -3,13 +3,14 @@ const course = express();
 import * as controllers from './index';
 import * as validation from './validation';
 import multer from 'multer';
-import {storage, videoFilter} from "../../Helpers/uploadFiles";
+import {imageFilter, storage, videoFilter} from "../../Helpers/uploadFiles";
 const upload = multer({ storage: storage, fileFilter: videoFilter }).single('video');
+const uploadAvatar = multer({ storage: storage, fileFilter: imageFilter }).single('avatar');
 
 course.get('/log/getAllCourses', controllers.getAllCourses);
 course.get('/getCourse', controllers.getCourse);
 course.post('/log/createUpdatePackage', validation.packageValidation, controllers.addCoursesToPackage);
-course.post('/log/createCourse', validation.courseValidation,  controllers.createCourse);
+course.post('/log/createCourse', validation.courseValidation, upload,  controllers.createCourse);
 course.post('/log/addLesson', validation.lessonValidation,  upload, controllers.createLesson);
 course.post('/log/newOrder', controllers.newOrder);
 
