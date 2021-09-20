77cbc1f33b014b0bdc67236f4218d4e3ba0e1dbd
Atomm94 <atom.makaryan.94@gmail.com>
2021-09-20 18:13:21 +0400

    new update

--- config.js
+++ config.js
@@ -9,12 +9,12 @@ const MONGO_HOSTNAME = '127.0.0.1';
 const MONGO_PORT = '27017';
 const MONGO_DB = 'wigAcademy';
 // "mongodb+srv://At11:atmak11@cluster0.d1re6.mongodb.net/wigAcademy?retryWrites=true&w=majority"
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+//const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
 const Stripe_Key = 'sk_test_51IaFWMHlASHs5y6L1umdBqkvs63efYNDyOsuRjf6RnNwjt0ym5NdesANBjWSjx5MY9nSQaiaZ8ISnO6N6KP2AHv900RJbpWelv';
 const stripe = require("stripe")(Stripe_Key);
 
-mongoose.connect(url, {
+mongoose.connect("mongodb+srv://At11:atmak11@cluster0.d1re6.mongodb.net/wigAcademy?retryWrites=true&w=majority", {
     useFindAndModify: true,
     useCreateIndex: true,
     useNewUrlParser: true,
