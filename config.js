166ea107607718f902587a388b3fc7e5a3a2f51b
Atomm94 <atom.makaryan.94@gmail.com>
2021-09-22 15:26:35 +0400

    new update

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 import env from 'dotenv';
 import mongoose from 'mongoose';
 import superAdminModel from "./Models/superAdmin";
+import jsonwebtoken from "jsonwebtoken";
 env.config();
 
 const MONGO_USERNAME = 'pablo';
@@ -9,12 +10,12 @@ const MONGO_HOSTNAME = '127.0.0.1';
 const MONGO_PORT = '27017';
 const MONGO_DB = 'wigAcademy';
 // "mongodb+srv://At11:atmak11@cluster0.d1re6.mongodb.net/wigAcademy?retryWrites=true&w=majority"
-//const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+const url = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
 const Stripe_Key = 'sk_test_51IaFWMHlASHs5y6L1umdBqkvs63efYNDyOsuRjf6RnNwjt0ym5NdesANBjWSjx5MY9nSQaiaZ8ISnO6N6KP2AHv900RJbpWelv';
 const stripe = require("stripe")(Stripe_Key);
 
-mongoose.connect("mongodb+srv://At11:atmak11@cluster0.d1re6.mongodb.net/wigAcademy?retryWrites=true&w=majority", {
+mongoose.connect(url, {
     useFindAndModify: true,
     useCreateIndex: true,
     useNewUrlParser: true,
@@ -22,6 +23,24 @@ mongoose.connect("mongodb+srv://At11:atmak11@cluster0.d1re6.mongodb.net/wigAcade
 })
     .then(_ => console.log('Database connect successfully!'))
     .catch(err => console.log(`Database connect error: ${err}`))
+
+    (async function () {
+        try {
+            const findSuperAdmin = await superAdminModel.find();
+            if (!findSuperAdmin) {
+                let obj = {
+                    fullName: 'Pablo Kuemin',
+                    email: process.env.SUPER_ADMIN_EMAIL,
+                    password: process.env.SUPER_ADMIN_PASSWORD
+                }
+                await superAdminModel.create(obj);
+                console.log('Super admin registered!');
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    })
+
 
 export {
     stripe
