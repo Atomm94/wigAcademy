621a3d94ed4c1272844d8131db87018c66b404f1
Atomm94 <atom.makaryan.94@gmail.com>
2021-11-09 17:34:12 +0400

    Validation bud fix v-1

--- Api/Course/router.js
+++ Api/Course/router.js
@@ -12,7 +12,7 @@ course.get('/getAllCourses', controllers.getAllCourses);
 course.get('/getCourse', controllers.getCourse);
 course.post('/log/createUpdatePackage', validation.packageValidation, controllers.addCoursesToPackage);
 course.post('/log/createCourse', uploadAvatar, validation.courseValidation,  controllers.createCourse);
-course.post('/log/addLesson', validation.lessonValidation,  upload, controllers.createLesson);
+// course.post('/log/addLesson', validation.lessonValidation,  upload, controllers.createLesson);
 course.post('/log/newOrder', controllers.newOrder);
 
 export default course;
